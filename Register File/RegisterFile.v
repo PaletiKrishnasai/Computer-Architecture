f5dceef6fdee71086479fa7f5032634f49ab6278
Paleti Krishnasai <paletikrishnasai245@gmail.com>
2021-01-24 02:24:50 +0530

    Update RegisterFile.v

--- Register File/RegisterFile.v
+++ Register File/RegisterFile.v
@@ -27,15 +27,11 @@
 // rising edge of the clock if 'RegWrite' signal is high. The D-flip flops in 
 // the register file are positive-edge (rising-edge) triggered. (You have to use 
 // this information to generate the write-clock properly.) 
-//
-// NOTE:-
-// We will design the register file such that the contents of registers do not 
-// change for a pre-specified time before the falling edge of the clock arrives 
-// to allow for data multiplexing and setup time.
+
+
 
 module RegisterFile(ReadRegister1, ReadRegister2, WriteRegister, WriteData, RegWrite, Clk, ReadData1, ReadData2);
 
-	/* Please fill in the implementation here... */ 
 	input [4:0] ReadRegister1,ReadRegister2,WriteRegister;
 	input [31:0] WriteData;
 	input RegWrite,Clk;
@@ -43,30 +39,31 @@ module RegisterFile(ReadRegister1, ReadRegister2, WriteRegister, WriteData, RegW
 	output reg [31:0] ReadData1,ReadData2;
 	
 	
-	//reg [31:0] Registers = new reg[32];
 	reg [31:0] Registers [0:31];
 	
-	initial begin
-		Registers[0] <= 32'h00000000;
-		Registers[8] <= 32'h00000000;
-		Registers[9] <= 32'h00000000;
-		Registers[10] <= 32'h00000000;
-		Registers[11] <= 32'h00000000;
-		Registers[12] <= 32'h00000000;
-		Registers[13] <= 32'h00000000;
-		Registers[14] <= 32'h00000000;
-		Registers[15] <= 32'h00000000;
-		Registers[16] <= 32'h00000000;
-		Registers[17] <= 32'h00000000;
-		Registers[18] <= 32'h00000000;
-		Registers[19] <= 32'h00000000;
-		Registers[20] <= 32'h00000000;
-		Registers[21] <= 32'h00000000;
-		Registers[22] <= 32'h00000000;
-		Registers[23] <= 32'h00000000;
-		Registers[24] <= 32'h00000000;
-		Registers[25] <= 32'h00000000;
-		Registers[29] <= 32'd252; // this value should point to the top of data memory, dont forget byte addressing
+	initial 
+	 begin
+		// $display("init");
+		Registers[0] <= 32'd23;
+		Registers[8] <= 32'd26;
+		Registers[9] <= 32'd35;
+		Registers[10] <= 32'd95;
+		Registers[11] <= 32'd65;
+		Registers[12] <= 32'd78;
+		Registers[13] <= 32'd14;
+		Registers[14] <= 32'd38;
+		Registers[15] <= 32'd74;
+		Registers[16] <= 32'd21;
+		Registers[17] <= 32'd49;
+		Registers[18] <= 32'd50;
+		Registers[19] <= 32'd13;
+		Registers[20] <= 32'd18;
+		Registers[21] <= 32'd24;
+		Registers[22] <= 32'd63;
+		Registers[23] <= 32'd81;
+		Registers[24] <= 32'd99;
+		Registers[25] <= 32'd96;
+		Registers[29] <= 32'd252;
 		Registers[31] <= 32'b0;
 	end
 	
@@ -82,10 +79,46 @@ module RegisterFile(ReadRegister1, ReadRegister2, WriteRegister, WriteData, RegW
 	
 	always @(negedge Clk)
 	begin
+		//$display("run");
 		ReadData1 <= Registers[ReadRegister1];
 		ReadData2 <= Registers[ReadRegister2];
 	end
 	
 	
+
+endmodule
+
+module top;
+
+	reg [4:0] ReadRegister1,ReadRegister2,WriteRegister;
+	reg [31:0] WriteData;
+	reg RegWrite=1,Clk;
+	wire [31:0] ReadData1,ReadData2;
+	RegisterFile RF1(ReadRegister1, ReadRegister2, WriteRegister, WriteData, RegWrite, Clk, ReadData1, ReadData2);
+	initial  
+	begin
+		#0 Clk = 1;
+		#10 Clk = 0;
+		#10 Clk = 1;
+		#10 Clk = 0;
+		#10 Clk = 1;
+		#10 Clk = 0;
+		#10 Clk = 1;
+		#10 Clk = 0;
+
+	end
+	initial begin
+		WriteData = 32'd525;
+		ReadRegister1 = 5'd8;
+		ReadRegister2 = 5'd9;
+		WriteRegister = 5'd10;
+		#30 ReadRegister1 = 5'd10; ReadRegister2 = 5'd9;
+	end
+	initial begin
+		$monitor($time, "ALU Input1=%d ALU Input2=%d CLK=%d \n",ReadData1,ReadData2,Clk);
+
+	end
+
+
 
 endmodule
